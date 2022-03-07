3b86903abc31f0a34e9f05871ed35d750e5de824
nikiopta <72725100+nikiopta@users.noreply.github.com>
2022-03-07 17:03:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,31 +2,22 @@ name = input("hi what your name? ")
 print("hi", name)
 yd = input("how you doing? (for best result say good or not good!) ")
 if yd == "good" or yd == "awesome" or yd == "great":
-    print("nice to hear!")
+    print("nice to hear,")
+    print(name, "have a nice day!")
     input()
     exit(0)
 if yd == "not good" or yd == "awful" or yd == "angry" or yd == "bad" or yd == "Negative":
     print("sorry to hear!")
-    df = input("do you have friends? (yes or no only!) ")
-    if df == "no":
-        print("sorry to hear")
-        bf = input("do you wanna be friends? (yes or no only!) ")
-        if bf == "no":
-            print("why not? but i hope tomorrow i am going to see you bey bey")
-            input()
-            exit(0)
-        if bf == "yes":
-            print("so we going meet tomorrow!")
-            print("we now bff!")
-            input()
-            exit(0)
-    if df == "yes":
-        af = input("do you angry at them? (yes or no only!) ")
-        if af == "yes":
-            print("so do something you love its make you happy!")
-            input()
-            exit(0)
-        if af == "no":
-            print("why you don't spend time with them? the going to make you happy!")
-            input()
+    input("what happen? ")
+    hy = input("does something can help you? (yes or no) ")
+    if hy == "yes":
+        wis = input("what it is?")
+        print("amm you can look it on google something like: how to", wis, "or something like that")
+        print("happy to help", name)
+        print("bey")
+        input()
+        exit(0)
+    if hy == "no":
+        print("its ok i recommend to [lzy some games talk to your friends or something like that its help me!")
+        input()
         exit(0)
