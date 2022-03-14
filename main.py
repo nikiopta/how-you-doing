cba8d08297975f650f738cd1bd7a069490cd6258
nikiopta <72725100+nikiopta@users.noreply.github.com>
2022-03-14 19:58:03 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
-while True:
-    name = input("hi what your name? ")
-    print("hi", name)
-    yd = input("how you doing? (good or not good only!) ")
-    if yd in ('good', 'not good'):
-        break
-if yd == "good":
-    print("nice to hear,")
-    print(name, "have a nice day!")
-    input()
-    exit(0)
-if yd == "not good":
-    print("sorry to hear!")
-    input("what happen? ")
-    hy = input("does something can help you? (yes or no) ")
-    if hy == "yes":
-        wis = input("what it is?")
-        print("amm you can look it on google something like: how to", wis, "or something like that")
-        print("happy to help", name)
-        print("bey")
-        input()
-        exit(0)
-    if hy == "no":
-        print("its ok i recommend to [lzy some games talk to your friends or something like that its help me!")
-        input()
-        exit(0)
+from tkinter import *
+
+import tkinter.simpledialog
+
+import tkinter.messagebox
+
+root = Tk()
+w = Label(root, text="my program")
+w.pack()
+
+tkinter.messagebox.showinfo("hi", "hi and welcome")
+name = tkinter.simpledialog.askstring("name", "what is your name?")
+yd = tkinter.simpledialog.askstring("feels", "how to you feel? (good not good)")
+if yd in "good":
+    output1 = "nice to hear", name
+    tkinter.messagebox.showinfo("done", output1)
+elif yd in "not good":
+    output2 = "sorry to hear", name
+    tkinter.messagebox.showinfo("done", output2)
+    output4 = "does something can help you? (yes or no)"
+    hy = tkinter.simpledialog.askstring("help?", output4)
+    if hy in "yes":
+        wis = tkinter.simpledialog.askstring("what it is?", "what it is?")
+        output5 = "amm you can look it on google something like: how to", wis, "or something like that"
+        tkinter.messagebox.showinfo("amm", output5)
+    elif hy in "no":
+        output6 = "its ok i recommend to play some games talk to your friends or something like that its help me!"
+        tkinter.messagebox.showinfo("sorry", output6)
 else:
-    print("what do you want to say?")
-    input()
-    exit(0)
+    output3 = "I did not understand that"
+    tkinter.messagebox.showinfo("rest the app", output3)
+root.mainloop()
